ca6f95001341aa04af445b195b6599743d1ebb73
angelorobson <helloween123>
2021-02-24 13:43:50 -0300

    create row layout

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "8feecd98f6a842b19d19542756b17fea"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
